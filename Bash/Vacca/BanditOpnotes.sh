373e26dfa3e242db514f1be7bbfc572b6a63a99e
christopher-vacca <128409876+christopher-vacca@users.noreply.github.com>
2023-04-03 07:12:35 -0400

    Add files via upload

---
+++ Bash/Vacca/BanditOpnotes.sh
@@ -0,0 +1,107 @@
+#!/bin/bash
+
+Level 1 password - NH2SXQwcBdpmTEzi3bvBHMM9H66vVXjL
+
+Level 2 password - rRGizSaX8Mk1RTb1CNQoXTcYZWU6lgzi
+
+Level 3 password - aBZ0W5EmUfAf7kHTQeOwd8bauFJ2lAiG
+
+Level 4 password - 2EW7BBsr6aMMoJ2HjW067dm8EgX26xNe
+
+Level 5 password - lrIWWI6bB37kxfiCQZqUdOIYfr6eEeqR
+
+#find $HOME -type f -size 1033c ! -executable -exec file {} + | grep ASCII
+Level 6 password - P4L4vucdmLnm8I7Vl7jG1ApGSfjYKqJU
+
+find / -type f -size 33c -group bandit6 -user bandit7 2>/dev/null
+Level 7 password - z7WtoNQU2XfjmMtWA8u5rN4vzqu4v99S
+
+Level 8 password - TESKZC0XvTetK0S9xNwm25STk5iWrBvP
+
+Level 9 password - EN632PlfYiZbn3PhVK3XOGSlNInNE00t
+
+Level 10 password - G7w8LIi6J3kTb8A7j9LgrywtEUlyyp6s
+
+Level 11 password - 6zPeziLdR2RKNdNYFNb6nVCKzphlXHBM
+
+Level 12 password - JVNBBFSmZwKKOP0XbFXOoW8chDz5yVRv 
+
+xxd command will reverse a hexdump
+cat data.txt | xxd -r > data
+file data (to find out information about the file which in this case is gzip compressed)
+mv data data2.gz#rename the file to .gz extension
+gzip -d data2.gz to decompress it
+file data2 (again to find info in this case bzip2)
+mv data2 data3.bz
+bzip2 -d data3.bz
+...
+...
+mv data4 data5.tar
+tar -xf data5.tar
+mv data5.bin data6.tar
+tar -xf data6.tar
+Level 13 password - wbWdlBxEir4CaE8LaPhauuOo6pwRmrDw
+
+Level 14 password - 
+-----BEGIN RSA PRIVATE KEY-----
+MIIEpAIBAAKCAQEAxkkOE83W2cOT7IWhFc9aPaaQmQDdgzuXCv+ppZHa++buSkN+
+gg0tcr7Fw8NLGa5+Uzec2rEg0WmeevB13AIoYp0MZyETq46t+jk9puNwZwIt9XgB
+ZufGtZEwWbFWw/vVLNwOXBe4UWStGRWzgPpEeSv5Tb1VjLZIBdGphTIK22Amz6Zb
+ThMsiMnyJafEwJ/T8PQO3myS91vUHEuoOMAzoUID4kN0MEZ3+XahyK0HJVq68KsV
+ObefXG1vvA3GAJ29kxJaqvRfgYnqZryWN7w3CHjNU4c/2Jkp+n8L0SnxaNA+WYA7
+jiPyTF0is8uzMlYQ4l1Lzh/8/MpvhCQF8r22dwIDAQABAoIBAQC6dWBjhyEOzjeA
+J3j/RWmap9M5zfJ/wb2bfidNpwbB8rsJ4sZIDZQ7XuIh4LfygoAQSS+bBw3RXvzE
+pvJt3SmU8hIDuLsCjL1VnBY5pY7Bju8g8aR/3FyjyNAqx/TLfzlLYfOu7i9Jet67
+xAh0tONG/u8FB5I3LAI2Vp6OviwvdWeC4nOxCthldpuPKNLA8rmMMVRTKQ+7T2VS
+nXmwYckKUcUgzoVSpiNZaS0zUDypdpy2+tRH3MQa5kqN1YKjvF8RC47woOYCktsD
+o3FFpGNFec9Taa3Msy+DfQQhHKZFKIL3bJDONtmrVvtYK40/yeU4aZ/HA2DQzwhe
+ol1AfiEhAoGBAOnVjosBkm7sblK+n4IEwPxs8sOmhPnTDUy5WGrpSCrXOmsVIBUf
+laL3ZGLx3xCIwtCnEucB9DvN2HZkupc/h6hTKUYLqXuyLD8njTrbRhLgbC9QrKrS
+M1F2fSTxVqPtZDlDMwjNR04xHA/fKh8bXXyTMqOHNJTHHNhbh3McdURjAoGBANkU
+1hqfnw7+aXncJ9bjysr1ZWbqOE5Nd8AFgfwaKuGTTVX2NsUQnCMWdOp+wFak40JH
+PKWkJNdBG+ex0H9JNQsTK3X5PBMAS8AfX0GrKeuwKWA6erytVTqjOfLYcdp5+z9s
+8DtVCxDuVsM+i4X8UqIGOlvGbtKEVokHPFXP1q/dAoGAcHg5YX7WEehCgCYTzpO+
+xysX8ScM2qS6xuZ3MqUWAxUWkh7NGZvhe0sGy9iOdANzwKw7mUUFViaCMR/t54W1
+GC83sOs3D7n5Mj8x3NdO8xFit7dT9a245TvaoYQ7KgmqpSg/ScKCw4c3eiLava+J
+3btnJeSIU+8ZXq9XjPRpKwUCgYA7z6LiOQKxNeXH3qHXcnHok855maUj5fJNpPbY
+iDkyZ8ySF8GlcFsky8Yw6fWCqfG3zDrohJ5l9JmEsBh7SadkwsZhvecQcS9t4vby
+9/8X4jS0P8ibfcKS4nBP+dT81kkkg5Z5MohXBORA7VWx+ACohcDEkprsQ+w32xeD
+qT1EvQKBgQDKm8ws2ByvSUVs9GjTilCajFqLJ0eVYzRPaY6f++Gv/UVfAPV4c+S0
+kAWpXbv5tbkkzbS0eaLPTKgLzavXtQoTtKwrjpolHKIHUz6Wu+n4abfAIRFubOdN
+/+aLoRQ0yBDRbdXMsZN/jvY44eM+xRLdRVyMmdPtP8belRi2E2aEzA==
+-----END RSA PRIVATE KEY-----
+chmod 600 for a private key
+ssh -i (specifys a private key instead of a password)
+fGrHPx402xGC7U7rXKDaxiWFTOiF0ENq
+
+To send a message to a specific port:
+echo "message" | nc localhost 30000
+Level 15 password - jN2kgmIXJ6fShzhT2avhotn4Zcka6tnt
+
+Send a message to a specific port but using SSL encryption:
+echo "password" | openssl s_client -connect localhost:30001 -ign_eof
+Level 16 password - JQttfApK4SeyHwDlI9SXGR50qclOAil1 
+
+nmap -p 31000-32000 localhost
+PORT      STATE SERVICE
+31046/tcp open  unknown
+31518/tcp open  unknown
+31691/tcp open  unknown
+31790/tcp open  unknown
+31960/tcp open  unknown
+echo "password" | openssl s_client -connect localhost:eachport -ign_eof
+
+Level 17 password - f9wS9ZUDvZoo3PooHgYuuWdawDFvGld2
+
+diff passwords.new passwords.old
+Level 18 password - hga5tuuCLF6fFzUpnagiMN8ssu9LFrdg
+
+Level 19 password - awhqfNnAbc1naukrpqDYcF95h7HoMTrC
+
+Level 20 password - VxCazJaVykI6W36BkBU0mJTCM8rR95XT
+
+
+
+
+
+
