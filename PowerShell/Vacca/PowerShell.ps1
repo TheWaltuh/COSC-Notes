6a63dc490c9596dece972cc4d98b73af6f79b328
christopher-vacca <128409876+christopher-vacca@users.noreply.github.com>
2023-04-04 13:50:52 -0400

    Update PowerShell.ps1

--- PowerShell/Vacca/PowerShell.ps1
+++ PowerShell/Vacca/PowerShell.ps1
@@ -113,6 +113,105 @@ fruit-output | sort     #sorts because is is written to the output of that cmdle
   
 #------------------------------------------------------------------------------------------------------------
 
+    write-host "variables"
+    
+#microsoft loves money $$$
+
+Get-Variables                                       #Lists all variables
+Get-ChildItem variable:                             #^
+
+$x = 10
+$X                                                  #10     Case insensitive to call
+
+remove-variable x                                   #self explanatory
+del variable:x                                      #same ^
+
+Get-Variable <var no dollar signs>
+Get-Childitem variable:<var no dollar sign>         #these would print what the var is equal to
+
+$x, $y, $z = "one", "two", "three"                  #x="one" y="two" z="three"
+$x = $y = $z = "one"                                #x y and z = "one"
+
+Test-Path <path>                                    #verifys path exists
+
+Test-Path variable:<var>                            #returns True or False if variable exists or not
+
+    write-host "Variable Expansion"
+
+$name = "Joe"
+write-host "Hello $name"                            #Hello Joe 
+write-host 'Hello $name'                            #Hello $name
+
+$hour = (Get-Date).hour
+$remain = 24 - $hour
+$remain                                             #hours remaining in the day
+
+$day = (get-date).dayofyear
+$daysremain = 365 - $day
+$daysremain                                         #Returns days remaining in the year
+
+(Get-Date).Hour.GetType()                           #GetType() method can tell what type a variable is
+
+    write-host "Automatic Variables"
+    
+$args                                               #Array of undeclared parameters
+$false                                              #False
+$true                                               #True
+$input                                              #Enumerator of all input passed to a function
+$matches                                            #Hashtable of the current match for a 'match'
+$null                                               #Black Hole
+$profile                                            #String that contains path to the default powershell profile
+    
+$PWD                                                #Current working Directory
+
+    write-host "Enviromental Variables"
+
+Get-Childitem env:                                  #lists all enviromental variables
+
+$env:<name>                                         #how to call and enviromental variable
+
+cd $env:HOMEPATH                                    #changes to the directory that is the users home directory
+
+
+    write-host "Variable Types"
+    
+[string]                                            #sefl explanatory
+[char]                                              #16-bit single unicode character
+[byte]                                              #8-bit unsigned character
+[int]                                               #32-bit signed integer
+[long]                                              #64-bit signed integer
+[single]                                            #32-bit floating poiint number
+[double]                                            #64-bit floating point number
+[float]                                             #32-bit floating point number
+[datetime]                                          #Date and Time
+[array]                                             #self explanatory
+
+(86.2).GetType().name                               #Double
+([float](86.2)).GetType().name                      #Single
+
+$var = Read-Host; [int]$var                         #User input is always read as a string and must be TYPECASTED
+
+write-host "Numbers ending in .5 always round to the nearest even integer"
+
+[int]5.5                                            #6
+[int]6.5                                            #6
+[int]4.5                                            #4
+
+[int]5.3                                            #5
+[int]5.7                                            #6
+
+
+
+
+
+
+
+#-------------------------------------------------------------------------------------------------------------
+
+
+
+
+
 ##############################################################################################################
 
 <#
@@ -123,16 +222,3 @@ Order of PoSh Cmdlet
 4.Path Enviroment Variables
 #>
 
-
-<#
-cmdlets we use in class:
-
-Get-Process                   #lists all processes
-Get-Childitem                 #directory listing
-
-
-
-
-
-
-#>
