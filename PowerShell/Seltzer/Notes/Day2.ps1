14c33e1de43e5a7e10f838bd52866cee85d17611
PSeltzer <128486642+PSeltzer@users.noreply.github.com>
2023-04-05 08:37:11 -0400

    Update Day2.ps1

--- PowerShell/Seltzer/Notes/Day2.ps1
+++ PowerShell/Seltzer/Notes/Day2.ps1
@@ -1,1 +1,8 @@
+###Comands
 
+###Script Block
+$myblock = { Get-service | format-table name, status }  #gets list of services and outputs to a table showing name and status
+& $myblock                                              #how you run a scrip block
+                                                        #everytime called it runs again, so goes through the commands inside again
+ 
+$<script block name> = { <commands to be in block> }    #basic syntax
