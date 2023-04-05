afddd3ca3d90da7ad7ee9f076f7ddd6bc2a5e3f1
PSeltzer <128486642+PSeltzer@users.noreply.github.com>
2023-04-05 08:38:55 -0400

    Update and rename Day1.ps1 to Notes.ps1

--- PowerShell/Seltzer/Notes/Notes.ps1
+++ PowerShell/Seltzer/Notes/Notes.ps1
@@ -221,4 +221,29 @@ Order of Powershell Cmd Search Order Execution
 
 #>
 
-#########################################################################################
+##########################################################################################################################################################
+
+                                                                  #Start of Day 2#
+                                                                  
+##########################################################################################################################################################
+
+###Script Block
+$myblock = { Get-service | format-table name, status }  #gets list of services and outputs to a table showing name and status
+& $myblock                                              #how you run a scrip block
+                                                        #everytime called it runs again, so goes through the commands inside again
+ 
+$<script block name> = { <commands to be in block> }    #basic syntax
+
+
+
+
+
+
+
+
+
+
+
+
+
+
