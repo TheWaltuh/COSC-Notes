94ec235bb77ffb3a138b82a5f3a426fe420b3ce2
PSeltzer <128486642+PSeltzer@users.noreply.github.com>
2023-04-06 09:20:46 -0400

    Update Day2Exs.ps1

--- PowerShell/Seltzer/Exercises/Day2Exs.ps1
+++ PowerShell/Seltzer/Exercises/Day2Exs.ps1
@@ -3,6 +3,9 @@ Practical Exercise: The Pipeline
 1. Display the start time of the earliest and latest running processes
 Get-Process | Select-Object -Property StartTime | Sort-Object StartTime | Select-Object -First 2 -Last 1
 
+Get-Process | Where-Object{$_.StartTime} | Measure-Object -Property StartTime -Minimum -Maximum | Select-Object -Property Minimum, Maximum
+#ALTERNATE WAY TO DO IT BY GETTING ONLY OBJECTS WITH A STARTTIME AND THEN GOING ABOUT FILTERING
+
 2. Identify a cmdlet that returns the current date and time then using this cmdlet and Select-object, display only the current day of the week
 Get-Date | Select-Object -Property DayOfWeek
 
@@ -31,8 +34,8 @@ Practical Exercise: Custom Object
 $data = [PSCustomObject]@{
     ComputerName = ((Get-WmiObject -class Win32_ComputerSystem).Name)
     OperatingSystem = ((Get-WmiObject -class Win32_OperatingSystem).name)
-    Version = ((Get-WmiObject -class Win32_OperatingSystem).Version)
-    Manufacturer = ((Get-WmiObject -class Win32_ComputerSystem).Manufacturer)
+    Version = ((Get-WmiObject -class Win32_BIOS).Version)
+    Manufacturer = ((Get-WmiObject -class Win32_BIOS).Manufacturer)
     Disks = (Get-WmiObject -class Win32_LogicalDisk).__Path
 }
 
@@ -46,20 +49,26 @@ $line1 = "Do you have model number: MT5437 for john.doe@sharklasers.com?"
 $line2 = "What model number for john.doe@sharklasers.com?"
 $pattern = 'MT(\d{4})'
 
-if ($line1 -match $pattern) {
-    $mat = $matches[0]
-    Write-Output "$Mat is the model number"
-}
-else {
-    Write-Output "No model number was found"
-}
-
-if ($line2 -match $pattern) {
-    Write-Output "$Mat is the model number"
-}
-else {
-    Write-Output "No model number was found"
-}
+switch($pattern) {
+   {$line1 -match $_} {
+      $line1 -match $pattern | out-null
+      $mat = $matches[0]
+      Write-Output "$Mat is the model number"
+   }
+   {$line2 -match $_} {
+       $line2 -match $pattern | out-null
+       $mat = $matches[0]
+       Write-Output "$Mat is the model number"
+   }
+   
+    {$line1 -notmatch $_} {
+      $line1 -notmatch $pattern | out-null
+      Write-Output "No model number was found in line 1"
+   }
+   {$line2 -notmatch $_} {
+      $line2 -notmatch $pattern | out-null
+      Write-Output "No model number was found in line 2"
+   }}
 
 2.Check both lines for model numbers and report individually the line and model number if found.
 $line1 = "Do you have model number: MT5437 for john.doe@sharklasers.com?"
@@ -103,8 +112,15 @@ Part 1
 $arr = ("notepad", "MSEdge", "MSpaint")
 for ($i=0; $i -lt $arr.length; $i++) {
     start-process $arr[$i]
-    get-process | select-object $arr[$i]
+    get-process $arr[$i]
     stop-process -name $arr[$i]
+}
+
+###Alternate way to do same thing (much simpler)
+foreach($item in $arr) {
+start-process $item
+get-process $item
+stop-process -name $item
 }
 
 Part 2
@@ -129,6 +145,19 @@ for ($i=0; $i -lt $arr.length; $i++) {
     start-process $arr[$i]
     get-process | Where-Object -FilterScript {$_.ProcessName -eq $array[$i]} | Select-Object Id | Out-File -FilePath C:\Users\student\Desktop\test.txt -Append
     stop-process -name $arr[$i]
+}
+
+
+$arr = "notepad", "MSEdge", "MSpaint"
+
+foreach($item in $arr) {
+start-process $item
+get-process $item
+(get-process $item).id | where-object {$item.name -ne "idle"} >> procs.txt
+}
+$cat = (get-content .\procs.txt)
+foreach($line in $cat) {
+stop-process -id $line
 }
 
 Part 3
@@ -159,8 +188,13 @@ for ($i=0; $i -lt $arr.length; $i++) {
       get-process | Where-Object -FilterScript {$_.ProcessName -eq $array[$i]} | Select-Object Id,ProcessName,StartTime,TotalProcessorTime,PageMemorySize
 }
    
-   
-   
+###ALTERNATE AND SLIGHTLY MORE SIMPLE WAY TO DO IT
+foreach($item in $arr) {
+start-process $item
+get-process $item | Sort-object ID | format-table -property ID, ProcessName,Starttime,TotalProcessorTime,VirtualMemorySize,Workingset64
+stop-process -name $item
+}
+
    
    
    
