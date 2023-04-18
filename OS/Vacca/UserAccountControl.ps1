a6b20b3a18b9fdf28b59a09a6d7a0d61835f80d3
christopher-vacca <128409876+christopher-vacca@users.noreply.github.com>
2023-04-18 10:43:32 -0400

    Update UserAccountControl.ps1

--- OS/Vacca/UserAccountControl.ps1
+++ OS/Vacca/UserAccountControl.ps1
@@ -1,1 +1,15 @@
+HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System      #reg location for UAC
+Prompt Colors:
+    Red - Application or publisher blocked by group policy
+    Blue & gold - Administrative application
+    Blue - Trusted and Authenticode signed application
+    Yellow - Unsigned or signed but not trusted application
+    
+The prompt will show if: (requires sysinternals)
+./strings C:\Windows\System32\*.exe -accepteula | select-string -SimpleMatch "autoelevate"
+./sigcheck -m C:\Windows\System32\slui.exe -accepteula | Select-String -SimpleMatch "level"
+Trusted dir
+
+
+
 
