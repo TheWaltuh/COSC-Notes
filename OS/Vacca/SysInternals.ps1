a203100e8434e6ef9255dbaa724951fc88e47635
christopher-vacca <128409876+christopher-vacca@users.noreply.github.com>
2023-04-18 10:21:36 -0400

    Update SysInternals.ps1

--- OS/Vacca/SysInternals.ps1
+++ OS/Vacca/SysInternals.ps1
@@ -1,1 +1,29 @@
+#####################################################################################
+"SysInternals"
+#####################################################################################
+net use * http://live.sysinternals.com
+or
+$wc = new-object System.Net.WebClient
+$wc.DownloadFile("https://download.sysinternals.com/files/SysinternalsSuite.zip",
+"$pwd\SysinternalsSuite.zip")
+Expand-Archive SysinternalsSuite.zip
+rm SysInternalsSuite.zip
+cd SysInternalsSuitels
+
+
+#Procmon - create a capture | filter for data
+.\Procmon.exe -accepteula
+
+#AutoRuns
+.\autoruns.exe
+
+#Process Explorer
+.\procexp.exe
+
+#TCP View
+.\tcpview.exe
+
+
+
+
 
