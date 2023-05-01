9e3e328a6f55a1098bce3bb4fa26035f9d25908a
christopher-vacca <128409876+christopher-vacca@users.noreply.github.com>
2023-05-01 10:39:07 -0400

    Update Day1.sh

--- Networking/Vacca/Day1.sh
+++ Networking/Vacca/Day1.sh
@@ -1,1 +1,32 @@
 #!/bin/bash
+
+OSI Model
+
+Layer 1 - Physical
+  The actual physical devices and mediums
+Layer 2 - Data Link
+  Logical information about the information called a frame
+  A frame will have a: DestMac,SrcMac,Ethertype
+  EtherType:
+    0x0800 IPv4
+    0x0806 Arp
+      requests and replys
+      stp is to prevent a broadcast storm
+    0x86DD  IPv6
+    0x8100  VLAN tag
+      extra bits in the header
+  Switches
+    Ports can be seperated into VLANS
+    switches can connect vlans to eachother with a trunk
+    ports can be shutdown, restrict, or protect
+Layer 3 - Network
+  IPv4
+  IPv6
+  ICMP
+  Routers
+    Routers have routing tables
+    
+Layer 4 - Trsnsport
+  TCP
+  UDP
+
