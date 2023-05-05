0ed2236469c5a51e1447a38e3697d35a64ea56a1
PSeltzer <128486642+PSeltzer@users.noreply.github.com>
2023-05-05 10:24:19 -0400

    Update Network_Reconnaissance.ps1

--- Networking/Seltzer/Network_Reconnaissance.ps1
+++ Networking/Seltzer/Network_Reconnaissance.ps1
@@ -3,7 +3,8 @@
                                                                #Network Reconnaissance#
 
 ############################################################################################################################################################
-
+                                CAN USE duckduckgo TO SEARCH FOR AN IP AND CIDER TO GET INFO ON THE NETWORK RANGE
+                                ex in duckduckgo search 10.10.0.40/27
 #Types of Recon
 https://git.cybbh.space/net/public/raw/master/modules/networking/slides/images/Recon2.png
     #Active
@@ -71,6 +72,7 @@ https://git.cybbh.space/net/public/raw/master/modules/networking/slides/images/R
           #Supplements base queries
           dig axfr @nsztm1.digi.ninja zonetransfer.me
                        <website>    
+          dig axfr @ns1.google.com www.google.com
         #Host History
           #netcraft
           #wayback machine
@@ -86,15 +88,67 @@ https://git.cybbh.space/net/public/raw/master/modules/networking/slides/images/R
 #Network Scanning
   #Scanning Strategy
       #Remote to Local
+          #refers to a remote host, outside the boundary of a specific network, performing some sort of scan on hosts internal to an enterprise network
       #Local to Remote
+          #occurs when a host, within the administrative control of an enterprise network, scans systems outside the network boundary
       #Local to Local
+          #refers to a host that scans systems within the boundaries of an enterprise in which it resides
       #Remote to Remote
+          #does not depend on certain boundaries
+          #mainly refers to world wide scanning campaigns
+  #Scanning approach
+      #Aim
+          #Wide Range Target Scan
+              #typically little human interaction involved (auto-rooters, automated to scan a network for vulnerable machines)
+          #Target Specific Scan
+              #focused scanning of a predetermined target host or network (botnet scanning or low and slow scan)
+      #Method
+          #Single Source Scan
+              #operates from a one to many fashion
+          #Distributed Scan
+              #multiple systems in a union to scan a network or host of interest
+  #Tools
+      fping
+      hping3
+      arping
+      nmap
+      netcat
+#Active External Network Recon: Host Discovery
+  #Broadcast Ping/Ping Sweep
+      fping -g -a 10.1.0.0/24
+      nmap –sn 10.0.0.0/24                          #was -sP which is now deprecated
+      for i in {1..254}; do ping -c 1 -W 1 10.1.1.$i | grep 'from'; done
+  #ARP Scan
+      sudo arp-scan --interface=eth0 --localnet
+      sudo arp-scan --interface=eth0 10.1.0.0/24
+      
+      arping –c 1 –i eth0 172.16.32.2 (can only scan a single host, results vary)
+      
+      for ip in $(sew 1 254) ; do if ping -c 1 10.1.0.$ip>/dev/null; then echo "10.1.0.$ip UP"; fi ;done
+      
+      nmap –PR 172.16.32.2                          #legitimate scan that often does not show results
+      nmap -PR -6 fe80::f816:3eff:fed9:5116/64      #takes a long time to run
+                                #For Examples of all the different types of scans see FG
+#NMAP Basics
+  sudo nmap 
+            -n #disable name resoultion
+            -P #print off the ping code
+  sudo nmap -Pn 10.10.1.0,5,55,70
+        #scans for multi ips in one scan
 
-
-
-
-
-
+#Subnetting Chart
+        128       64        32        16        8       4        2       1
+             +=
+        128       192       224       240       248     252      254     255
+      ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+      | /1    |   /2    |   /3    |   /4    |   /5    |  /6   |  /7   |  /8  |
+      ------------------------------------------------------------------------
+A /8  | /9    |   /10   |   /11   |   /12   |   /13   |  /14  |  /15  |  /16 |
+      ------------------------------------------------------------------------
+B /16 | /17   |   /18   |   /19   |   /20   |   /21   |  /22  |  /23  |  /24 |
+      ------------------------------------------------------------------------
+C /24 | /25   |   /26   |   /27   |   /28   |   /29   |  /30  |  /31  |  /32 |
+      ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 
 
