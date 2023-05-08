89afe37045919ab01114d60246da66b3e348112b
PSeltzer <128486642+PSeltzer@users.noreply.github.com>
2023-05-08 14:42:34 -0400

    Update Data_Transfer.ps1

--- Networking/Seltzer/Data_Transfer.ps1
+++ Networking/Seltzer/Data_Transfer.ps1
@@ -97,18 +97,60 @@
                 nc Host_1 <port1> 0< mypipe | nc -lp <port2> 1> mypipe
                 nc Host_1 <port1> 0< mypipe | nc Host_2 <port2> 1> mypipe
         
+                                                   #For examples basic map
+                    IH ---------(22)> Host_1 ---------(22)> Host_2 (80)---------(22,23,80)> Host_3 (22)
+        
 #Tunneling
     #SSH
         #multichannel connection
-        #Local Port Forwarding
+        #SSH Local Port Forwarding
           ssh -p <optional alt port> <user>@<pivot ip> -L <local bind port>:<tgt ip>:<tgt port> -NT
           ssh -L <local bind port>:<tgt ip>:<tgt port> -p <alt port> <user>@<pivot ip> -NT
           #local port forwarding through a local port
             ssh student@localhost -p 1111 -L 2222:172.16.82.106:80 -NT
             #when on a box can use localhost instead of retyping the ip again
-        #Dynamic Port Forwarding
-          
-        
+        #SSH Dynamic Port Forwarding
+          #Bsic Syntax
+            ssh -D <port> -p <alt port> <user>@<pivot ip> -NT
+          #Proxychains default port is 9050
+          #Creates a dynamic socks4 proxy that interacts alone, or with a previously established remote or local port forward
+          #Allows the use of scripts and other userspace programs through the tunnel.
+          #1 step forwarding
+            ssh student@172.16.82.106 -D 9050 -NT
+          #2 step forwarding
+            ssh <user>@172.16.82.106 -L 1111:10.10.0.40:22 -NT
+            ssh student@localhost -D 9050 -p 1111 -NT
+          #exs
+            IH> ssh user@host_1 -L 1111:Host_2:22 -NY
+            IH> ssh -p user@localhost -D 9050 -NT
+            IH> proxychains ftp Host_3
+            IH> proxychains nc
+            IH> proxychains nmap -Pn
+            IH> proxychains curl
+                  #Default port for proxychains is 9050
+                      #why we use it with dynamic ports
+        #Remote Port Forwarding
+          #Basic Syntax
+              ssh -p <optional alt port> <user>@<remote ip> -R <remote bind port>:<tgt ip>:<tgt port> -NT
+              or
+              ssh -R <remote bind port>:<tgt ip>:<tgt port> -p <alt port> <user>@<remote ip> -NT
+          #SSH Remote and local port forwarding
+              Blue Private Host-1:
+              ssh student@172.16.82.106 -R 1111:localhost:22 -NT
+              
+              Internet Host:
+              ssh student@172.16.82.106 -L 2222:localhost:1111 -NT
+              
+              Internet Host:
+              ssh localhost -p 2222
+              
+              #Creates a remote port on a remote machine, staging a connection
+              #also creates a local pot on the local  host to connect to the previously staged connection 
+          #exs
+              Blue Host-1
+              ssh student@10.10.0.40 -R 1111:localhost:80 -N
+                  #creates a remote port on remotes local host that forwards to the target specified
+                  
 
         
 
