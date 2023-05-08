fb5f96bc85a07b69ee8cecaafd716a78eec0481c
christopher-vacca <128409876+christopher-vacca@users.noreply.github.com>
2023-05-08 09:02:50 -0400

    Update DataTransfer.sh

--- Networking/Vacca/DataTransfer.sh
+++ Networking/Vacca/DataTransfer.sh
@@ -5,10 +5,22 @@ tftp - udp 69; very simple communication
 ftp (21(control) 20(data))
   active- client initiates a connection to port 21, server initiates the data transfer
   passive- client initiates the connection and data transfer
-sftp - like ftp but run over ssh (tcp 22)
+  ### Demo ###
+  ftp <ip>
+  Name: (<ip>:name): <name>
+  password:
+  help
+  active(default)
+  passive
+  pwd
+  put <file>        #copy a file from the client to the server in the current dir
+  ls
+sftp - like ftp (interactive) but run over ssh (tcp 22)
 ftps - file transfer protocol secure (tcp 443)
-scp (tcp 22)
-
+scp (tcp 22) - non interactive
+          download a file: scp <user>@<ip>:<file> <dir>
+          upload a file: scp <file> <user>@<ip>:<dir>
+          copy file from rhost to rhost: scp -3 <user>@<srcip>:<filepath> <user>@<dstip>:<dir>
 
 
 
