bd5a14722289df38148d620352f6efccc7510cfd
christopher-vacca <128409876+christopher-vacca@users.noreply.github.com>
2023-05-08 08:48:11 -0400

    Create DataTransfer.sh

---
+++ Networking/Vacca/DataTransfer.sh
@@ -0,0 +1,24 @@
+##########################################
+          Data Transfer
+##########################################
+tftp - udp 69; very simple communication
+ftp (21(control) 20(data))
+  active- client initiates a connection to port 21, server initiates the data transfer
+  passive- client initiates the connection and data transfer
+sftp - like ftp but run over ssh (tcp 22)
+ftps - file transfer protocol secure (tcp 443)
+scp (tcp 22)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
