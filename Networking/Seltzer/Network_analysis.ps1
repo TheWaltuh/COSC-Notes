f5d040110da93faf11a188a17c4fc2dd22805919
PSeltzer <128486642+PSeltzer@users.noreply.github.com>
2023-05-11 09:31:38 -0400

    Update Network_analysis.ps1

--- Networking/Seltzer/Network_analysis.ps1
+++ Networking/Seltzer/Network_analysis.ps1
@@ -37,9 +37,58 @@
         #IPv4 and IPv6 Statistics
         #Expert Information
 
+#Network Data Types
+    #Full Packet Capture Data
+    #Session Data
+        #sflow
+        #NetFlow
+    #Statistical Data
+    #Packet String Data
+    #Alert Data
+    #Log Data
+    #Data Collection Devices
+        #Sensors
+            #In-Line
+                #like a MITM type thing
+                Pros:
+                  #Can cause dropping of sus packets
+                Cons:
+                  #if sensor goes down, whole network can go down
+                  #Is visible (shows up as hop)
+            #Passive
+                #Where you just copy data and send out another line
+                #Invisible from enmey perspective
+                #Does not impact network
+    #Data Collection Methods
+        #TAPS
+            #Like the spy movie shit with tapping phone lines, etc
+        #SPAN
+            #no additional hardware needed
+        #ARP Spoofing (MITM)
+            #make data go through your device and then copy it
 
-
-
+#Anaomaly detection
+    #Indicator of Attack (IOA)
+        #Proactive
+        #a series of actions that are suspicous together
+        #Focus of intent
+        #looks for what must happen
+            #code execution, persistance, lateral movement, etc.
+    #Indicator of Compromise (IOC)
+        #Reactive
+        #forensic evidence
+        #Provides information that canchange
+            #Malware, ip addresses, exploits, signatures
+#Indicators
+    #.exe/executable files
+    #NOP sled
+        #No Operation = NOP
+    #Repeated Letters
+    #Well Known Signatures
+    #Mismatched Protocols
+    #Unusual traffic
+    #Large amounts of traffic/ unusual times
+    
 
 
 
