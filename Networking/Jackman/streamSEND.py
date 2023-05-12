64c29832ac2e88c1aa9ec887335ec98c2e112600
pout <69238112+pout@users.noreply.github.com>
2023-05-12 14:01:20 -0400

    Update streamSEND.py

--- Networking/Jackman/streamSEND.py
+++ Networking/Jackman/streamSEND.py
@@ -5,13 +5,13 @@ import socket
 # This can also be accomplished by using s= socket.socket() due to AF_INET and SOCK_STREAM being default
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-ipaddr = '127.0.0.1'
-port = 54321
+ipaddr = '172.16.1.15'
+port = 5309
 
 s.connect((ipaddr, port))
 
 # To send a string as a bytes-like object, add the prefix b to the string. \n is used to go to the next line
-s.send(b'Hello is it me you\'re looking for\n')
+s.send(b'953e720e688941b15b72c098022c51c3')
 
 # It is recommended that the buffersize used with recvfrom is a power of 2 and not a very large number of bits
 response, conn = s.recvfrom(1024)
