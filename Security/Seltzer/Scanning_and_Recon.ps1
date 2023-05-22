1e0f16aad4b02c7dc87c91017a7bb17b69892f83
PSeltzer <128486642+PSeltzer@users.noreply.github.com>
2023-05-22 09:34:40 -0400

    Update Scanning_and_Recon.ps1

--- Security/Seltzer/Scanning_and_Recon.ps1
+++ Security/Seltzer/Scanning_and_Recon.ps1
@@ -59,8 +59,11 @@
     #Used for port discoverny/enumeration
     #Has its own scripting engine
         /usr/share/nmap/scripts
+            #dir with thousands of scripts in it
+            #will be given ones to use so we dont have to parse all them
     
-
+#Robots.txt file
+    #file for web pages that contains area not allowed to be scraped by people looking at web pages
 
 
 
