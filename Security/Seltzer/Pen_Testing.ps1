d373296ba65b654a3696aed019956011e37061f1
PSeltzer <128486642+PSeltzer@users.noreply.github.com>
2023-05-22 08:57:00 -0400

    Update Pen_Testing.ps1

--- Security/Seltzer/Pen_Testing.ps1
+++ Security/Seltzer/Pen_Testing.ps1
@@ -4,4 +4,31 @@
 
 ################################################################################################################################################################################
 
+#Why do
+    #allows us to see possible faults in our system
+    #a "self assessment" of network
+
+#Phase 1
+    #Mission Definition 
+        #Define Mission goals and targets
+        #Determine scope of mission
+        #Define RoE
+#Phase 2
+    #Recon
+        #Information is gathered about the target through public sources
+#Phase 3
+    #Footprinting
+        #Accumulasting data through scanning and/or interaction with the target/target resources
+#Phase 4
+    #Exploitation and Initial access
+        #Gain an Initial foothold on network
+#Phase 5
+    #Post-Exploitation
+        #Establish persistence
+        #escalate privileges
+        #Cover your tracks
+        #Exfiltrate target data
+#Phase 6
+    #Document Mission
+        #Document and report mission details
 
