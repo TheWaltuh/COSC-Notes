eb87c7e7abc0277fb74d26b33f044ecb90eae487
christopher-vacca <128409876+christopher-vacca@users.noreply.github.com>
2023-06-08 08:38:00 -0400

    Create DryRun.py

---
+++ Security/Vacca/DryRun.py
@@ -0,0 +1,27 @@
+#############################################
+                  Dry Run
+#############################################
+methodology:
+  step 1: always have notes open (draw.io)
+    
+  Host Discovery
+    
+
+  Port Discovery
+    nmap -Pn (-sT) -T4 <ip> -p80,22,4444,2222,4242,9999,3389,8080
+  
+  Port Interrogation
+  
+  
+
+
+
+
+
+
+
+
+
+
+
+
